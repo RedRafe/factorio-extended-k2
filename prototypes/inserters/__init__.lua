5ed48f87d37e562fbf1d3a4dc596df94ab9baa85
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-03-05 12:46:56 +0100

    start inserters Ptypes

---
+++ prototypes/inserters/__init__.lua
@@ -0,0 +1,4 @@
+require("superior-inserter")
+require("superior-filter-inserter")
+require("superior-long-inserter")
+require("superior-long-filter-inserter")
---
+++ prototypes/inserters/superior-long-inserter.lua
@@ -0,0 +1,155 @@
+data:extend({
+  {
+    type = "inserter",
+    name = "fek2-superior-long-inserter",
+    icon = "__base__/graphics/icons/long-handed-inserter.png",
+    icon_size = 64, icon_mipmaps = 4,
+    flags = {"placeable-neutral", "placeable-player", "player-creation"},
+    minable = {mining_time = 0.1, result = "fek2-superior-long-inserter"},
+    max_health = 160,
+    corpse = "long-handed-inserter-remnants",
+    dying_explosion = "long-handed-inserter-explosion",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    collision_box = {{-0.15, -0.15}, {0.15, 0.15}},
+    selection_box = {{-0.4, -0.35}, {0.4, 0.45}},
+    damaged_trigger_effect = hit_effects.entity(),
+    pickup_position = {0, -2},
+    insert_position = {0, 2.2},
+    energy_per_movement = "14KJ",
+    energy_per_rotation = "14KJ",
+    stack = true,
+    rotation_speed = 0.06,
+    extension_speed = 0.105,
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      drain = "1kW"
+    },
+    fast_replaceable_group = "long-handed-inserter",
+    vehicle_impact_sound = sounds.generic_impact,
+    open_sound = sounds.machine_open,
+    close_sound = sounds.machine_close,
+    working_sound = sounds.inserter_long_handed,
+    hand_base_picture =
+    {
+      filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-hand-base.png",
+      priority = "extra-high",
+      width = 8,
+      height = 34,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-hand-base.png",
+        priority = "extra-high",
+        width = 32,
+        height = 136,
+        scale = 0.25
+      }
+    },
+    hand_closed_picture =
+    {
+      filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-hand-closed.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-hand-closed.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    hand_open_picture =
+    {
+      filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-hand-open.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-hand-open.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    hand_base_shadow =
+    {
+      filename = "__base__/graphics/entity/burner-inserter/burner-inserter-hand-base-shadow.png",
+      priority = "extra-high",
+      width = 8,
+      height = 33,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/burner-inserter/hr-burner-inserter-hand-base-shadow.png",
+        priority = "extra-high",
+        width = 32,
+        height = 132,
+        scale = 0.25
+      }
+    },
+    hand_closed_shadow =
+    {
+      filename = "__base__/graphics/entity/burner-inserter/burner-inserter-hand-closed-shadow.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/burner-inserter/hr-burner-inserter-hand-closed-shadow.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    hand_open_shadow =
+    {
+      filename = "__base__/graphics/entity/burner-inserter/burner-inserter-hand-open-shadow.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/burner-inserter/hr-burner-inserter-hand-open-shadow.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    platform_picture =
+    {
+      sheet =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-platform.png",
+        priority = "extra-high",
+        width = 46,
+        height = 46,
+        shift = {0.09375, 0},
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-platform.png",
+          priority = "extra-high",
+          width = 105,
+          height = 79,
+          shift = util.by_pixel(1.5, 7.5-1),
+          scale = 0.5
+        }
+      }
+    },
+    circuit_wire_connection_points = circuit_connector_definitions["inserter"].points,
+    circuit_connector_sprites = circuit_connector_definitions["inserter"].sprites,
+    circuit_wire_max_distance = inserter_circuit_wire_max_distance,
+    default_stack_control_input_signal = inserter_default_stack_control_input_signal
+  },
+})
